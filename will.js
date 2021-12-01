68a279a3350c1ec5d8084309c712e0cae02e64ca
William Berry <76190311+wberry86@users.noreply.github.com>
2021-11-30 20:17:03 -0600

    added picture and button

---
+++ will.js
@@ -0,0 +1,11 @@
+// constants
+const FIRST_NAME = "William ";
+const LAST_NAME = "Berry";
+const MIDDLE_NAME = "Trousdale ";
+
+const contestant = FIRST_NAME + MIDDLE_NAME + LAST_NAME;
+
+
+console.log(contestant, contestant.length);
+
+

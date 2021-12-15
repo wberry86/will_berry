d8f33834a93bbf2c9dd2df9e8d08274aaf105f1b
William Berry <76190311+wberry86@users.noreply.github.com>
2021-12-14 20:14:56 -0600

    added practive js

--- nc_assets/js/will.js
+++ nc_assets/js/will.js
@@ -5,48 +5,166 @@ const MIDDLE_NAME = "Trousdale ";
 
 const contestant = FIRST_NAME + MIDDLE_NAME + LAST_NAME;
 
-
 console.log(contestant, contestant.length);
 
-
 function helloContestant(name) {
-    console.log("Hello " + name + ", what would you like to accomplish today?");
+  console.log("Hello " + name + ", what would you like to accomplish today?");
 }
 
 helloContestant("Big Willy T");
 
-
 document.getElementById("btn").addEventListener("click", displayDate);
 
 function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
+  document.getElementById("demo").innerHTML = Date();
 }
 
+const myCar = {
+  make: "BMW",
+  model: "328i",
+  color: "dark green",
+  year: 1998,
+};
+
+const myPets = {
+  Lyla: {
+    age: "senior",
+    breed: "walker hound",
+    personality: "goofy and lovable",
+    friends: ["Frankie", "Achilles", "Riley"],
+  },
+  Frankie: {
+    age: "middle aged",
+    breed: "jack russell terrier",
+    personality: "mostly nice with some aggresive issues",
+    friends: ["Lyla", "Achilles", "Riley"],
+  },
+};
+
+const arr = [
+  ["will", "Berry"],
+  ["john", "Lennon"],
+  ["paul", "McArtney"],
+  ["george", "Harrison"],
+  ["ringo", "Star"],
+];
+
+const myArray = [];
+let i = 10;
+
+do {
+    myArray.push(i);
+    i++;
+} while (i < 20);
+
+console.log(myArray);
 
 
 
-function caseInSwitch(val) {
-    let answer = "";
+// recursive function 
 
-    switch (val) {
-        case 1:
-            return "Alright thats a start!"
-            break;
-        case 2:
-            return "Now your getting the hang of it!"
-            break;
-        case 3:
-            return "Looking like a pro!"
-            break;
-        case 4: 
-            return "Magni-fucking-ificint!"
-            break;
-        default:
-            return "Whoa lets keep it in the ballpark buddy"
-            break;
+function sum(arr, n) {
+    if (n <= 0) {
+        return 0;
+    } else {
+        return sum(arr, n - 1) + arr[n - 1];
     }
-    return answer;
 }
 
-console.log(caseInSwitch(40));
+console.log(sum([2, 3, 4], 2));
 
+
+
+// profile lookup
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+
+  function lookUpProfile(name, prop) {
+      for (let i = 0; i < contacts.length; i++) {
+          if (contacts[i].firstName === name) {
+              if (contacts[i].hasOwnProperty(prop)) {
+                  return contacts[i][prop];
+              } else {
+                  return "No such proppy prop";
+              }
+          }
+      }
+      return "No contact fool!"
+  }
+
+  
+  console.log(lookUpProfile("Kristian", "likes"));
+
+
+
+// Random Numbers
+function randomWholeNumber() {
+    return Math.floor(Math.random() *101);
+}
+
+console.log(randomWholeNumber());
+
+
+// random numbers within a range
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(randomRange(18, 74));
+
+
+// parse integer
+const bond = parseInt("1.777");
+console.log(bond);
+
+function convertToInteger(str) {
+    return parseInt(str);
+}
+
+console.log(convertToInteger("888.9"));
+
+
+
+// ternary operators
+var age = 32;
+
+if (age >= 18) {
+    console.log("Congrats! You are officially an adult.");
+} else {
+    console.log("Hey you kid get outa my store!");
+}
+
+console.log((age >= 18) ? "Hey look at you your a full grown adult" : "Beat it kid I got important things to do");
+
+function checkSign(num) {
+    return (num > 0) ? "Hey look at that... its positive!"
+    : (num < 0) ? "Oh my goodness looks like we got a negative nancy!"
+    : "Its a Zero!"
+}
+
+console.log(checkSign(0));
